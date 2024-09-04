8053f08ce4b283b6d1021537f1da30be977cf85d
gabriele.lana <gabriele.lana@gmail.com>
2024-09-04 12:19:31 +0200

    More robust local end2end test, cleanup after error

--- test/local/run.sh
+++ test/local/run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -xou pipefail
 
 # assert to be run in root directory
 if [[ ! -d $PWD/.git ]]; then
     echo "This script should be run in root directory" 1>&2
-    exit
+    exit 1
 fi
 
 # build docker image
 docker build -t "lrp:latest" .
 
-cd test/local
+cd test/local || {
+    echo "Cannot change directory to test/local" 1>&2
+    exit 1
+}
 
 # start test environment and make sure everything is up and running
-docker compose -p test-e2e -f ./docker-compose.yaml up --build --wait --wait-timeout 60
+docker compose -p test-e2e up --build --wait --wait-timeout 60 || {
+    docker compose -p test-e2e down --remove-orphans --volumes
+    echo "Cannot start docker compose environment" 1>&2
+    exit 1
+}
 while true; do
     curl -s --fail "http://localhost:41414/ping" > /dev/null 2>&1 && break
 done
@@ -25,6 +32,6 @@ k6 run --quiet --out json=test_results.json k6.js
 EXIT_CODE=$?
 
 # shut down everything
-docker compose -p test-e2e -f ./docker-compose.yaml down --remove-orphans --volumes
+docker compose -p test-e2e down --remove-orphans --volumes
 
 exit $EXIT_CODE
